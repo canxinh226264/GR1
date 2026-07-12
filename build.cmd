d7bdd83df0a4e4c241306e8ec790c19a4cd41309
canxinh226264 <Anh.C235636@sis.hust.edu.vn>
2026-07-12 17:59:02 +0700

    Delete build.cmd

--- build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-for %%I in ("%~dp0..\..") do set "OMNETPP_ROOT=%%~fI"
-call "%OMNETPP_ROOT%\opp_shell.cmd" -no-start -c "cd \"$HOME/samples/marl_wsn\" && opp_makemake -f --deep -o marl_wsn && make -j4 MODE=release"
-endlocal
