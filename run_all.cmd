9d46e373ffaed03707d2049e577e791450e06756
canxinh226264 <Anh.C235636@sis.hust.edu.vn>
2026-07-12 17:59:10 +0700

    Delete run_all.cmd

--- run_all.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-for %%I in ("%~dp0..\..") do set "OMNETPP_ROOT=%%~fI"
-call "%OMNETPP_ROOT%\opp_shell.cmd" -no-start -c "cd \"$HOME/samples/marl_wsn\" && ./marl_wsn.exe -u Cmdenv -c MARL && ./marl_wsn.exe -u Cmdenv -c CloudMARL && ./marl_wsn.exe -u Cmdenv -c SPMH && ./marl_wsn.exe -u Cmdenv -c LEACH && ./marl_wsn.exe -u Cmdenv -c Paper3D"
-endlocal
